576120fee95287d0a8956108e7dab033f5d59f35
lbsystem <lbsystem@126.com>
2022-05-28 23:25:20 +0800

    home

--- TESTsocket.py
+++ TESTsocket.py
@@ -3,13 +3,22 @@ import socket,time
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
 
-server_socket.bind(('',7890))
+server_socket.bind(('',7891))
 server_socket.listen(8)
 new_socket,addr = server_socket.accept()
+new_socket.setblocking(False)
 print("Start")
 while True:
-    
-    print(new_socket)
-    recv=new_socket.recv(1024).decode()
-    print(recv)
+    try:
+        time.sleep(2)
+        recv=new_socket.recv(1024).decode()
+    except Exception as e:
+        print(e)
+    else:
+          if recv:
+            print("fasdfasdfasdfasdfasdf",recv)
+          else:
+              print("00000000000000000")
+  
+        
     
